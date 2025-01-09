6534a9bd06112879979bb4cc2e5bde424b16a813
GD-Asoka <51798666+GD-Asoka@users.noreply.github.com>
2025-01-08 21:21:19 -0800

    fix

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,22 +1,3 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
-
-document.addEventListener('DOMContentLoaded', function () {
-  const images = document.querySelectorAll('.zoomable-image');
-  const overlay = document.getElementById('overlay');
-
-  images.forEach(image => {
-    image.addEventListener('click', function () {
-      overlay.style.display = 'flex';
-      const zoomedImage = document.createElement('img');
-      zoomedImage.src = image.src;
-      overlay.appendChild(zoomedImage);
-    });
-  });
-
-  overlay.addEventListener('click', function () {
-    overlay.style.display = 'none';
-    overlay.innerHTML = '';
-  });
-});
